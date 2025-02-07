1780167e17baa7572efcb759dade63e255e7a462
Oleksandr Zarudnyi <hedgar2017@gmail.com>
2025-02-07 12:56:53 +0800

    feat(evm): transient storage

--- src/evm/instructions/storage.rs
+++ src/evm/instructions/storage.rs
@@ -43,3 +43,40 @@ pub fn store<'ctx>(
     context.build_store(position_pointer, value)?;
     Ok(())
 }
+
+///
+/// Translates the transient storage load.
+///
+pub fn transient_load<'ctx>(
+    context: &mut Context<'ctx>,
+    position: inkwell::values::IntValue<'ctx>,
+) -> anyhow::Result<inkwell::values::BasicValueEnum<'ctx>> {
+    let position_pointer = Pointer::new_with_offset(
+        context,
+        AddressSpace::TransientStorage,
+        context.field_type(),
+        position,
+        "transient_storage_load_position_pointer",
+    )?;
+    let value = context.build_load(position_pointer, "transient_storage_load_value")?;
+    Ok(value)
+}
+
+///
+/// Translates the transient storage store.
+///
+pub fn transient_store<'ctx>(
+    context: &mut Context<'ctx>,
+    position: inkwell::values::IntValue<'ctx>,
+    value: inkwell::values::IntValue<'ctx>,
+) -> anyhow::Result<()> {
+    let position_pointer = Pointer::new_with_offset(
+        context,
+        AddressSpace::TransientStorage,
+        context.field_type(),
+        position,
+        "transient_storage_store_position_pointer",
+    )?;
+    context.build_store(position_pointer, value)?;
+    Ok(())
+}
